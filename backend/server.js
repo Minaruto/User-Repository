c8d83a92ed38213983cf56117fcd582c32c5dd98
MyroneGonsalves <myrone.gonsalves@outlook.com>
2023-12-26 11:18:28 +0400

    Created API Endpoint to fetch users from Github

--- backend/server.js
+++ backend/server.js
@@ -13,3 +13,28 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+// Search GitHub users
+app.post('/api/search', async (req, res) => {
+    const { searchText } = req.body;
+    const cacheKey = `search:${searchText}`;
+
+    // Try to fetch from cache
+    client.get(cacheKey, async (err, cachedData) => {
+        if (err) throw err;
+
+        if (cachedData) {
+            return res.json({ source: 'cache', data: JSON.parse(cachedData) });
+        } else {
+            // Fetch from GitHub API
+            try {
+                const response = await fetch(`https://api.github.com/search/users?q=${searchText}`);
+                const data = await response.json();
+                client.setex(cacheKey, 7200, JSON.stringify(data.items)); // Cache for 2 hours
+                res.json({ source: 'api', data: data.items });
+            } catch (error) {
+                res.status(500).json({ message: error.message });
+            }
+        }
+    });
+});
